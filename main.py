38e48327f075e9f63d9c1f1e5f766c43c01254c9
Easwara Krishnan S <easwarakrishnan2003@gmail.com>
2025-02-15 10:37:23 +0530

    updates

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ app = FastAPI()
 
 @app.get('/')
 def index():
-    return {'data' : {'Name' : 'Sarath','ID':'2981','Location':'Bangalore'}}
+    return {'data' : 'Lists'}
 
-@app.get('/about')
-def about():
-    return {'data':'About Page'}
+@app.get('/about/{id}')
+def about(id : int):
+    return {'data':id}
+
+@app.get('/about/{id}/comments')
+def about_comments(id):
+    return {'data' : {'1','2'}}
